5f3cce867c5720ebd3d5fd8e388d665e74abcf96
Daniel Breedeveld <dhbreedeveld@gmail.com>
2016-06-09 16:53:34 +0200

    Added main classes

---
+++ DEV02-04 - Assigment Design Patterns/Component.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEV02_04___Assigment_Design_Patterns
+{
+    interface Component
+    {
+        public void Update(float dt)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+
+        }
+    }
+}
---
+++ DEV02-04 - Assigment Design Patterns/Label.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEV02_04___Assigment_Design_Patterns
+{
+    class Label
+    {
+        private String text;
+        private SpriteFont font;
+
+        public Label(Vector2 p, string t, SpriteFont f)
+        {
+
+        }
+    }
+}

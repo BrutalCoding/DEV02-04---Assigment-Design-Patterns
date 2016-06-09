9072406b2238d952ee56b72e107f26b120564db0
Daniel Breedeveld <dhbreedeveld@gmail.com>
2016-06-09 16:22:53 +0200

    Added Factory design pattern

---
+++ DEV02-04 - Assigment Design Patterns/FormLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEV02_04___Assigment_Design_Patterns
+{
+    class FormLoader
+    {
+        private List<ElementType> elements;
+        public FormLoader(List<ElementType> elements)
+        {
+            this.elements = elements;
+        }
+
+        public GUIElementsFactory Load()
+        {
+
+        }
+    }
+}
---
+++ DEV02-04 - Assigment Design Patterns/GUIElementFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEV02_04___Assigment_Design_Patterns
+{
+    class GUIElementFactory
+    {
+        private ElementType type;
+        public GUIElementFactory(ElementType t)
+        {
+            this.type = t;
+        }
+
+        public GUIElement Load()
+        {
+
+        }
+    }
+}
